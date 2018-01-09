9c20059eda7123231b4469703c6a6e08b891fbb6
Alex Ginsberg <alex.ginsberg@richmond.edu>
2018-01-09 15:39:47 -0500

    Set up an emit whenever there is a state change; emit when a new client connects to the server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,19 @@ import Server from 'socket.io'
 /*
     Creates a Socket.io server and an HTTP server
 */
-export default function startServer() {
+export default function startServer(store) {
     const io = new Server().attach(8090)
+
+    // Whenever something in the store changes, emits it on the socket server
+    // Publishes the whole state to everybody when a change occurs
+    // It is possible to only send what is changed, but this is convenient 
+    store.subscribe(
+        () => io.emit('state', store.getState().toJS())
+    )
+
+    // connection event can be listend for, occurs when a client connects
+    // Emits the current state when a client connects
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS())
+    })
 }
